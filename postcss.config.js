21b5e0e383e5eec57cf7e9c36918b68f16c30dc8
AJ Troxell <aj.troxell@bunn.com>
2024-05-28 10:09:37 -0500

    added existing files

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    // 'postcss-import': {},
+    // 'postcss-preset-env': { stage: 4 },
+    // 'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
